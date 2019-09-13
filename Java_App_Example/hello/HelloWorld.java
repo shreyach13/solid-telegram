1fcc3dce7f7562d00294dce3cb1250377fb6065e
Thomas Schizas <thomas.schizas@contino.io>
2019-09-13 12:11:53 +0200

    Fixed code

--- Java_App_Example/hello/HelloWorld.java
+++ Java_App_Example/hello/HelloWorld.java
@@ -2,7 +2,6 @@ package hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+        System.out.println("Hello World!");
     }
 }
