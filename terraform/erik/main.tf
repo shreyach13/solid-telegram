6901f3212cf4127c3c27b81a59ce87d00ceea7c0
Erik Vermeer <erik.vermeer@shell.com>
2019-09-18 16:42:55 +0200

    Updated original main.tf

--- terraform/erik/main.tf
+++ terraform/erik/main.tf
@@ -2,16 +2,11 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_instance" "webserver" {
-  ami = "ami-0ac05733838eabc06"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ErikWebserver"
-  }
-}
-
 resource "aws_s3_bucket" "eriks-terraform" {
   bucket = "erik-terraform-bucket"
   region = "eu-central-1"
+  tags = {
+    Name = "ErikWebserver"
+    owner = var.owner
+  }
 }
