eacf5b6160b88899ff978f8b92b1e66110bec8bf
Nishant.Nikhil <nishant.nikhil@NishantNikhils-MacBook-Pro.local>
2019-09-18 13:19:50 +0200

    deleted security groups

--- terraform/nishant/securitygroups.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_security_group" "allow_tls_traffic" {
-  name        = "allow_tls"
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
