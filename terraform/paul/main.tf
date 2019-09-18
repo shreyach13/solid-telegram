784fcf1a273850a9eac09198d30f46073c7b7976
Paul.Broughton <paul.broughton@shell.com>
2019-09-18 13:39:13 +0200

    Updated main.tf with a variable, added tags to resources and added a new output for public_ip and all values for the webserver

---
+++ terraform/paul/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+variable "ownerName" {
+  default = "Paul"
+}
+
+resource "aws_instance" "webserver" {
+  ami	= "ami-00aa4671cbf840d82"
+  instance_type = "t2.micro"
+
+  tags = {
+      Name = "Pauls-Webserver"
+      owner = var.ownerName
+  }
+}
+
+resource "aws_s3_bucket" "pauls-terraform-state" {
+  bucket = "s3-terraform-state-paul"
+  region = "eu-central-1"
+  tags = {
+      owner = var.ownerName
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.webserver.public_ip}"
+  description = "This is my public IP"
+}
+
+output "webserver" {
+  value = "${aws_instance.webserver}"
+  description = "This is my webserver"
+}
