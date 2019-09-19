ad267f02d4a929924454bd78edefcb2f87fd2c11
Prasanth Sasidharan <prasanth.sasidharan@prasanths-mbp.clients.europe.shell.com>
2019-09-19 11:31:03 +0200

    updated file for terraform

--- terraform/prasanth/main.tf
+++ terraform/prasanth/main.tf
@@ -2,9 +2,10 @@ provider "aws" {
      region = "eu-central-1"
 }
 resource "aws_s3_bucket" "terraform" {
-      bucket = "prasanth-terraform"
+      bucket = "red3-prasanth-terraform"
       region = "eu-central-1"
       tags = {
+      Name = "Prasanth Bucket"      
       owner = var.owner
       }
 }
