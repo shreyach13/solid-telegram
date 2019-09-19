c4b2e7a7ad369e0da4df54be3ac533b1eaed8cf2
Prasanth Sasidharan <prasanth.sasidharan@prasanths-mbp.clients.europe.shell.com>
2019-09-19 11:32:08 +0200

    updated file for terraform

--- terraform/prasanth/with-remote-state/modules/compute/variable.tf
+++ terraform/prasanth/with-remote-state/modules/compute/variable.tf
@@ -2,3 +2,6 @@ variable "server_port"{
    description = "the port server will user for httpd request"
    default="8080"
    }
+variable "security_group_id" {
+   description="this is security group id"
+}
