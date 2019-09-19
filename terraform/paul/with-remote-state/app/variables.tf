20fc321ae8d0543203bee729c854bd60a6ad265d
Paul.Broughton <paul.broughton@shell.com>
2019-09-19 14:19:21 +0200

    Update after adding a remote git repository for a module

--- terraform/paul/with-remote-state/app/variables.tf
+++ terraform/paul/with-remote-state/app/variables.tf
@@ -2,3 +2,11 @@ variable "server_port" {
   description = "The port the server will use for HTTP requests"
   default="8080"
 }
+
+variable "from_port" {
+  default="8080"
+}
+
+variable "to_port" {
+  default="8080"
+}
