3f88b0df8f2eb0f02762e0b5179ab130b7c36b2d
Paul.Broughton <paul.broughton@shell.com>
2019-09-19 08:53:58 +0200

    Final state after terraform day1

---
+++ terraform/paul/with-remote-state/modules/compute/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami   = "ami-00aa4671cbf840d82"
+  instance_type = "t2.micro"
+  user_data = <<-EOF
+		#!/bin/bash
+		echo "Hello world!!!" > imdex.html
+		nohup busybox httpd -f -p "${var.server_port}" &
+		EOF
+}	
---
+++ terraform/paul/with-remote-state/modules/compute/variables.tf
@@ -0,0 +1,4 @@
+variable "server_port" {
+  description= "The port of the server for HTTP requests"
+  default="8080"
+}
