8f5f0a1af77193ff6669eadcc3ef69a3508ca5b3
Prasanth Sasidharan <prasanth.sasidharan@prasanths-mbp.clients.europe.shell.com>
2019-09-20 10:53:11 +0200

    added new file

---
+++ terraform/green-class-exercise/with-remote-state/modules/compute/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "jenkins_server" {
+     ami            = "ami-0d01b4444599bbd02"
+     instance_type  = "t2.micro"
+     security_groups = [var.security_group_id]
+     tags = {
+          Name = "greenteam-jenkinsserver"
+          Owner = "green-team"
+     }
+     }
