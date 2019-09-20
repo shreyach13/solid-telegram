0713d6a75e149f6b0b23b4b5a1e22e1e4d2bbef9
Paul.Broughton <paul.broughton@shell.com>
2019-09-20 14:14:51 +0200

    Latest files

---
+++ team_green/terraform/local-state/pipeline/pipeline-job/variables.tf
@@ -0,0 +1,11 @@
+variable "servers" {
+  default = {
+    "1-flinders" : "flinders",
+    "1-gascoyne" : "gascoyne",
+    "2-gascoyne" : "gascoyne",
+    "1-lachlan" : "lachlan",
+    "2-lachlan" : "lachlan",
+    "3-lachlan" : "lachlan",
+    "1-murray" : "murray",
+  }
+}
