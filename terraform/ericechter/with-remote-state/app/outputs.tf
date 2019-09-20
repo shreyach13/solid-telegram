169e0ab8cd6a4e45db2f9a3a2f02e0d4c1368682
Eric.Echter <eric.echter@EricEchters-MacBook-Pro.local>
2019-09-20 08:57:01 +0200

    First push today

--- terraform/ericechter/with-remote-state/app/outputs.tf
+++ terraform/ericechter/with-remote-state/app/outputs.tf
@@ -1,3 +1,8 @@
+/*output "security_group_name" {
+  description = "the security group name"
+  value       = module.security.security_group_name
+}*/
+
 output "security_group_name" {
   description = "the security group name"
   value       = module.security.security_group_name
