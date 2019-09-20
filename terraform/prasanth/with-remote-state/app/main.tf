0cb3448b0121e523f5350d038aa4a668bd8b3dc7
Prasanth Sasidharan <prasanth.sasidharan@Prasanths-MacBook-Pro.local>
2019-09-20 08:55:07 +0200

    updated terraform file

--- terraform/prasanth/with-remote-state/app/main.tf
+++ terraform/prasanth/with-remote-state/app/main.tf
@@ -1,9 +1,8 @@
 module "web_server"{
    source = "../modules/compute"
    server_port = var.server_port
-   security_group_id=module.security.security_group_id
+   security_group_id=module.security.security_group_name
   }
  module "security" {
-  source = "../modules/security"
+  source = "git@github.com:mhristof/security.git?ref=v3.0.0"
  }
-  
