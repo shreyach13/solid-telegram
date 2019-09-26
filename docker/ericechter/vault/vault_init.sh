ed34ec700cde88a404d38127a36c80d5c71ab45d
Eric.Echter <eric.echter@EricEchters-MacBook-Pro.local>
2019-09-26 07:36:14 +0200

    Cleaned up vault_init.sh using linter

--- docker/ericechter/vault/vault_init.sh
+++ docker/ericechter/vault/vault_init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-read KEY TOKEN <<< \
-  $(/home/app/vault operator init -key-shares 1 -key-threshold 1 | \
-  awk -F"1: | Token: " '$2 != "" {ORS=" "; print $2}')
-/home/app/vault operator unseal ${KEY}
-export VAULT_TOKEN=${TOKEN}
+read -r KEY TOKEN <<< \
+  "$(/home/app/vault operator init -key-shares 1 -key-threshold 1 | \
+  awk -F"1: | Token: " '$2 != "" {ORS=" "; print $2}')"
+/home/app/vault operator unseal "${KEY}"
+export VAULT_TOKEN="${TOKEN}"
 /home/app/vault secrets enable -path secret kv
 /home/app/vault kv put secret/foo value=bar
 
