96bf37001e537b1643e612a8844062d719ad781a
Olivia.Blaine <olivia.blaine@oliviabainesmbp.clients.europe.shell.com>
2019-10-02 11:36:10 +0200

    added practice

--- docker/oli/wdpractice/initialize.sh
+++ docker/oli/wdpractice/initialize.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 set -euox pipefail
 
-apt-get update
-apt-get install -y curl
-
 while ! curl http://vault:8200/v1/sys/health | grep initialized; do
   echo "Waiting to see if initialized"
   sleep 2
 done
-
-
 
 /vault operator init -key-shares 1 -key-threshold 1 > vault_out.txt
 UNSEALKEY=`grep "Unseal Key 1" vault_out.txt | awk '{print $NF}'`
